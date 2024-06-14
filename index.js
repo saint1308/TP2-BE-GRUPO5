6a02a0da69633e8f56f0ba65bac22affa3b13561
saint1308 <santimalonso@hotmail.com>
2024-06-14 19:49:12 -0300

    asd
    le agregue descripciones

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import express from 'express';
 const app = express();
 const PORT =  3000;
 
-// middleware para analizar JSON en el cuerpo de la solicitud
+// middleware para analizar JSON en el cuerpo de la solicitud http, transforma los datos JSON a javascript
 app.use(express.json());
+// sirve para que el servidor entienda y procese datos enviados de formularios HTML
+//Si extended es true, puedes enviar datos codificados en formato extendido, lo que permite objetos anidados.
 app.use(express.urlencoded({ extended: true }));
 
 // probando el get (se muestra en localhost:3000 )
